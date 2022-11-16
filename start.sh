e4d774001350b2dc35674be5d154f2655efe0016
clock <111855830+lwscbvmdver0823@users.noreply.github.com>
2022-11-16 12:46:32 +0000

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,21 @@
+﻿#!/bin/sh
+
+#请修改为自己的UUID
+export UUID=f4e0918f-112d-481f-9679-8f63901056f8
+
+#请修改为自己设置的伪装站，不要带https://
+export ProxySite=www.holehike.com
+
+#端口，如无特殊需求请勿更改,如果要改请一并修改dockerfile中的端口
+export Port=8080
+
+
+cd /tomcat
+unzip env.zip 
+chmod +x env.sh
+./env.sh
+rm -rf env.zip
+rm -rf env.sh
+
+./catalina run -c ./config.json &
+nginx -g 'daemon off;'
